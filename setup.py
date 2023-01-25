34d6dca2a9402612995daa5b7e8f378f1aa8c61d
Fluffy-Bean <michal-gdula@protonmail.com>
2023-01-25 16:46:13 +0000

    Add Download button Forgotten to update version :skull:

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='onlylegs',
-    version='210123',
+    version='250123',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
