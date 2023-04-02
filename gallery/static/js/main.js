91ea307b66d1a9af1e974454e09a1ed9521313fd
Fluffy-Bean <michal-gdula@protonmail.com>
2023-04-02 17:40:45 +0000

    Update version

--- gallery/static/js/main.js
+++ gallery/static/js/main.js
@@ -64,7 +64,7 @@ window.onload = function () {
                       'Using <a href="https://phosphoricons.com/">Phosphoricons</a> and ' +
                       '<a href="https://www.gent.media/manrope">Manrope</a> <br>' +
                       'Made by Fluffy and others with ❤️ <br>' +
-                      '<a href="https://github.com/Fluffy-Bean/onlylegs">V23.03.30</a>');
+                      '<a href="https://github.com/Fluffy-Bean/onlylegs">V23.04.02</a>');
         }
     }
 };
